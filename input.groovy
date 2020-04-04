224c58517886aa45675e5c521b975fb4da22e8d9
umar832 <59777479+umar832@users.noreply.github.com>
2020-04-04 12:16:23 -0500

    Create input.groovy

---
+++ input.groovy
@@ -0,0 +1,16 @@
+ode {
+	stage("Stage1"){
+		echo "hello"
+	}
+	stage("Stage2"){
+		echo "hello"
+	}
+	stage("Stage3"){
+		echo "hello"
+	}
+	stage("Ask for Input"){
+		input 'Should I proceed?'
+	}
+		stage("Stage5"){
+	}
+}
