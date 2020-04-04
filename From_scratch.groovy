c2409557c7fa8305e07158442330d4a415694c0e
Cloud User <centos@ip-172-31-94-137.ec2.internal>
2020-04-04 18:37:50 +0000

    add

--- From_scratch.groovy
+++ From_scratch.groovy
@@ -13,21 +13,21 @@ node {
 }
 	stage("Install Prerequiset"){
 		sh """
-		ssh  centos@jenkins_worker1.awsumar.com   sudo yum install httpd -y
+		ssh  centos@dev1.awsumar.com   sudo yum install httpd -y
 		"""
 }
 	stage("Copy Artifacts"){
 		sh """
 		scp -r * centos@jenkins_worker1.awsumar.com:/tmp
-		ssh centos@jenkins_worker1.awsumar.com                        sudo cp -r /tmp/index.html /var/www/html/
-		ssh centos@jenkins_worker1.awsumar.com                        sudo cp -r /tmp/style.css /var/www/html/
-		ssh centos@jenkins_worker1.awsumar.com                        sudo chown centos:centos /var/www/html/
-		ssh centos@jenkins_worker1.awsumar.com                         sudo chmod 777 /var/www/html/*
+		ssh centos@dev1.awsumar.com                        sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@dev1.awsumar.com                        sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@dev1.awsumar.com                        sudo chown centos:centos /var/www/html/
+		ssh centos@dev1.awsumar.com                         sudo chmod 777 /var/www/html/*
 
 		"""
 }
 	stage("Restart web server"){
-		sh "ssh centos@jenkins_worker1.awsumar.com             sudo systemctl restart httpd"
+		sh "ssh centos@dev1.awsumar.com             sudo systemctl restart httpd"
 }
 	stage("Slack"){
 		slackSend color: '#BADA55', message: 'Hello, World!'
