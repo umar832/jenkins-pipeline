9b793e98ce68abc3f47dcaa8ccdbaff796beb0a7
Cloud User <centos@ip-172-31-94-137.ec2.internal>
2020-04-04 18:20:27 +0000

    add

--- From_scratch.groovy
+++ From_scratch.groovy
@@ -19,17 +19,17 @@ node {
 	stage("Copy Artifacts"){
 		sh """
 		scp -r * centos@jenkins_worker1.awsumar.com:/tmp
-		ssh centos@jenkins_worker1.awsumar.com                        sudo cp  -r /tmp/index.html /var/www/html/
-		ssh centos@jenkins_worker1.awsumar.com                        sudo cp  -r /tmp/style.css /var/www/html/
-		ssh centos@jenkins_worker1.awsumar.com                        sudo chow centos:centos /var/www/html/
-		ssh centos@jenkins_worker1.awsumar.com                        sudo chmod 777  /var/www/html/*
+		ssh centos@jenkins_worker1.awsumar.com                        sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@jenkins_worker1.awsumar.com                        sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@jenkins_worker1.awsumar.com                        sudo chown centos:centos /var/www/html/
+		ssh centos@jenkins_worker1.awsumar.com                         sudo chmod 777 /var/www/html/*
 
 		"""
 }
 	stage("Restart web server"){
-		ssh "centos@jenkins_worker1.awsumar.com                        sudo systemctl restart httpd"
+		sh "ssh centos@${ENVIR}               sudo systemctl restart httpd"
 }
 	stage("Slack"){
-		slackSend color: 'BADA55', message: 'Hello Wordl!'
+		slackSend color: '#BADA55', message: 'Hello, World!'
 	}
 }
