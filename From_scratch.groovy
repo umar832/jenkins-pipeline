0898d29c28ebb96ade81ef9a93319f0190c57863
Cloud User <centos@ip-172-31-90-241.ec2.internal>
2020-04-09 21:27:01 +0000

    add

--- From_scratch.groovy
+++ From_scratch.groovy
@@ -4,15 +4,6 @@ node {
 		properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')),
 		// Below line triggers this job every minute
 		pipelineTriggers([pollSCM('* * * * * ')]),
-		parameters([
-			// Asks for Environment to Build
-			choice(choices: [
-			'dev1.awsumar.com', 
-			'qa1.awsumar.com', 
-			'stage1.awsumar.com', 
-			'prod1.awsumar.com'], 
-			description: 'Please choose an environment', 
-			name: 'ENVIR'),
 		])
 
 		// Pulls a repo from developer
@@ -22,23 +13,23 @@ node {
 		//Installs web server on different environment
 	stage("Install Prerequisites"){
 		sh """
-		ssh centos@${ENVIR}                 sudo yum install httpd -y
+		ssh centos@dev1.awsumar.com                sudo yum install httpd -y
 		"""
 	}
 		//Copies over developers files to different environment
 	stage("Copy artifacts"){
 		sh """
 		scp -r *  centos@${ENVIR}:/tmp
-		ssh centos@${ENVIR}                 sudo cp -r /tmp/index.html /var/www/html/
-		ssh centos@${ENVIR}                 sudo cp -r /tmp/style.css /var/www/html/
-		ssh centos@${ENVIR}				    sudo chown centos:centos /var/www/html/
-		ssh centos@${ENVIR}				    sudo chmod 777 /var/www/html/*
+		ssh centos@dev1.awsumar.com                sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@dev1.awsumar.com                sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@dev1.awsumar.com			    sudo chown centos:centos /var/www/html/
+		ssh centos@dev1.awsumar.com			    sudo chmod 777 /var/www/html/*
 		"""
 	}
 		//Restarts web server
 	stage("Restart web server"){
 		ws("tmp/") {
-			sh "ssh centos@${ENVIR}               sudo systemctl restart httpd"
+			sh "ssh centos@dev1.awsumar.com              sudo systemctl restart httpd"
 		}
 	}
 
