d94c1a833ee91221e9b277bbe50b48eb8e5fd105
Cloud User <centos@ip-172-31-90-241.ec2.internal>
2020-04-09 03:29:48 +0000

    add

--- From_scratch.groovy
+++ From_scratch.groovy
@@ -4,16 +4,6 @@ node {
 		properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5'))
 		// Below line triggers this job every minute
 		pipelineTriggers([pollSCM('* * * * *')])
-		parameters([
-			// Asks for Environment to Build
-			choice(choices: [
-			'dev1.awsumar.com', 
-			'qa1.awsumar.com', 
-			'stage1.awsumar.com', 
-			'prod1.awsumar.com'], 
-			description: 'Please choose an environment', 
-			name: 'ENVIR'),
-         
 		 ])
 
 		// Pulls a repo from developer
@@ -23,23 +13,23 @@ node {
 		//Installs web server on different environment
 	stage("Install Prerequisites"){
 		sh """
-		ssh centos@${ENVIR}                 sudo yum install httpd -y
+		ssh centos@jenkins_master.awsumar.com                 sudo yum install httpd -y
 		"""
 	}
 		//Copies over developers files to different environment
 	stage("Copy artifacts"){
 		sh """
-		scp -r *  centos@${ENVIR}:/tmp
-		ssh centos@${ENVIR}                 sudo cp -r /tmp/index.html /var/www/html/
-		ssh centos@${ENVIR}                 sudo cp -r /tmp/style.css /var/www/html/
-		ssh centos@${ENVIR}				    sudo chown centos:centos /var/www/html/
-		ssh centos@${ENVIR}				    sudo chmod 777 /var/www/html/*
+		scp -r *  centos@jenkins_master.awsumar.com:/tmp
+		ssh centos@jenkins_master.awsumar.com                 sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@jenkins_master.awsumar.com                 sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@jenkins_master.awsumar.com				    sudo chown centos:centos /var/www/html/
+		ssh centos@jenkins_master.awsumar.com				    sudo chmod 777 /var/www/html/*
 		"""
 	}
 		//Restarts web server
 	stage("Restart web server"){
 		ws("tmp/") {
-			sh "ssh centos@${ENVIR}               sudo systemctl restart httpd"
+			sh "ssh centos@jenkins_master.awsumar.com               sudo systemctl restart httpd"
 		}
 	}
 
