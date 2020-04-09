357301b045cf90a7e388e4e08b074f70f0c3d8e0
Cloud User <centos@ip-172-31-90-241.ec2.internal>
2020-04-09 02:46:47 +0000

    adad

--- From_scratch.groovy
+++ From_scratch.groovy
@@ -4,7 +4,7 @@ node {
 		properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5'))
 		
 		// Below line triggers this job every minute
-		pipelineTriggers([pollSCM('* * * * * ')])
+		pipelineTriggers([pollSCM('* * * * *')])
 		])
 
 		// Pulls a repo from developer
@@ -14,22 +14,22 @@ node {
 		//Installs web server on different environment
 	stage("Install Prerequisites"){
 		sh """
-		ssh centos@dev1.awsumar.com               sudo yum install httpd -y
+		ssh centos@jenkins_master.awsumar.com               sudo yum install httpd -y
 		"""
 	}
 		//Copies over developers files to different environment
 	stage("Copy artifacts"){
 		sh """
-		scp -r *  centos@dev1.awsumar.com:/tmp
-		ssh centos@dev1.awsumar.com                sudo cp -r /tmp/index.html /var/www/html/
-		ssh centos@dev1.awsumar.com                sudo cp -r /tmp/style.css /var/www/html/
-		ssh centos@dev1.awsumar.com			    sudo chown centos:centos /var/www/html/
-		ssh centos@dev1.awsumar.com			    sudo chmod 777 /var/www/html/*
+		scp -r *  centos@jenkins_master.awsumar.com:/tmp
+		ssh centos@jenkins_master.awsumar.com                sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@jenkins_master.awsumar.com                sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@jenkins_master.awsumar.com		    sudo chown centos:centos /var/www/html/
+		ssh centos@jenkins_master.awsumar.com		    sudo chmod 777 /var/www/html/*
 		"""
 	}
 		//Restarts web server
 	stage("Restart web server"){
-		sh "ssh centos@dev1.awsumar.com             sudo systemctl restart httpd"
+		sh "ssh centos@jenkins_master.awsumar.com             sudo systemctl restart httpd"
 	}
 
 		//Sends a message to slack
