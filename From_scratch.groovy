bad9165ee4ecf06c24734c86b537affd2ae5d975
Cloud User <centos@ip-172-31-90-241.ec2.internal>
2020-04-09 21:11:22 +0000

    add

--- From_scratch.groovy
+++ From_scratch.groovy
@@ -18,7 +18,7 @@ node {
 
 		// Pulls a repo from developer
 	stage("Pull Repo"){
-		checkout([$class: 'GitSCM', branches: [[name: '*/FarrukH']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/cool_website.git']]])
+		git 'https://github.com/farrukh90/cool_website.git'
 	}
 		//Installs web server on different environment
 	stage("Install Prerequisites"){
