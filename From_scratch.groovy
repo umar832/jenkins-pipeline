12134d85a7b0eb64a5e04afc8ffb4b1f707a93dc
Cloud User <centos@ip-172-31-85-209.ec2.internal>
2020-04-10 23:14:40 +0000

    add

--- From_scratch.groovy
+++ From_scratch.groovy
@@ -1,21 +1,42 @@
 node {
+	properties([
+		// Below line sets "Discard Builds more than 5"
+		properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')),  
+		// Below line triggers this job every minute
+		pipelineTriggers([pollSCM('* * * * * ')])
+		])
 
-properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')),
- pipelineTriggers([pollSCM('* * * * * ')]) 
- ])
-	stage("Stage1"){
-		echo "hello"
-}
-	stage("Stage2"){
-		echo "hello"
-}
-	stage("Stage3"){
-		echo "hello"
-}
-	stage("Stage4"){
-		echo "hello"
-}
-	stage("Stage5"){
-		echo "hello"
+		// Pulls a repo from developer
+	stage("Pull Repo"){
+		git 'https://github.com/farrukh90/cool_website.git'
+	}
+		//Installs web server on different environment
+	stage("Install Prerequisites"){
+		sh """
+		ssh centos@jenkins_master.awsumar.com                 sudo yum install httpd -y
+		"""
+	}
+		//Copies over developers files to different environment
+	stage("Copy artifacts"){
+		sh """
+		scp -r *  centos@djenkins_master.awsumar.com:/tmp
+		ssh centos@djenkins_master.awsumar.com                 sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@djenkins_master.awsumar.com                 sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@djenkins_master.awsumar.com				    sudo chown centos:centos /var/www/html/
+		ssh centos@djenkins_master.awsumar.com				    sudo chmod 777 /var/www/html/*
+		"""
+	}
+		//Restarts web server
+	stage("Restart web server"){
+		ws("tmp/") {
+			sh "ssh centos@${ENVIR}               sudo systemctl restart httpd"
+		}
+	}
+
+		//Sends a message to slack
+	stage("Slack"){
+		ws("mnt/"){
+			slackSend color: '#BADA55', message: 'Hello, World!'
+		}
 	}
 }
