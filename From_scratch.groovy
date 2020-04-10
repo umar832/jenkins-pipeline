bc5a473e3865ba75b43ed974249aefb95d3b511a
Cloud User <centos@ip-172-31-85-209.ec2.internal>
2020-04-10 23:52:01 +0000

    add

--- From_scratch.groovy
+++ From_scratch.groovy
@@ -1,12 +1,10 @@
 node {
 	properties([
 		// Below line sets "Discard Builds more than 5"
-		properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')),
+		properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
 		// Below line triggers this job every minute
 		pipelineTriggers([pollSCM('* * * * * ')])
-		parameters([
-			// Asks for Environment to Build
-			choice(choices: [
+		parameters([choice(choices: [
 			'dev1.awsumar.com', 
 			'qa1.awsumar.com', 
 			'stage1.awsumar.com', 
