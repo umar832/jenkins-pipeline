86aa24909ebe7498aaf5d66e887abd97da5398d4
Cloud User <centos@ip-172-31-85-209.ec2.internal>
2020-04-10 22:19:20 +0000

    add

---
+++ Pull_Repo.groovy
@@ -0,0 +1,33 @@
+node { 
+
+stage("Stage1"){ 
+// pull the repo and perform the task 
+git 'https://github.com/umar832/Cool_webiste.git' 
+
+} 
+
+stage("Stage2"){ 
+
+echo "hello" 
+
+} 
+
+stage("Stage3"){ 
+
+echo "hello" 
+
+} 
+
+stage("Stage4"){ 
+
+echo "hello" 
+
+} 
+
+stage("Stage5"){ 
+
+echo "hello" 
+
+} 
+
+} 
