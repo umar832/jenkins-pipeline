5be7e5054332820692232121f6bd72b5df400a55
Cloud User <centos@ip-172-31-85-209.ec2.internal>
2020-04-10 22:13:17 +0000

    add

--- From_scratch.groovy
+++ From_scratch.groovy
@@ -4,8 +4,8 @@ node {
 		properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), pipelineTriggers([pollSCM('* * * * * ')])])
 	
 	
-	// Pulls a repo from developer
-	stage("Pull Repo"){
+	// Pulls a Repository from developer
+	stage("Pull Repository"){
 		git 'https://github.com/umar832/Cool_webiste.git'
 	}
 		//Installs web server on different environment
