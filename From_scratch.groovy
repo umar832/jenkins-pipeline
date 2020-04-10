47cf05c9eff508532d4b4fbbada46a0e7d42c860
Cloud User <centos@ip-172-31-85-209.ec2.internal>
2020-04-10 23:02:00 +0000

    add

--- From_scratch.groovy
+++ From_scratch.groovy
@@ -1,33 +1,44 @@
 node {
 	properties([
 		// Below line sets "Discard Builds more than 5"
-		properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), pipelineTriggers([pollSCM('* * * * * ')])])
-     
-	 // pull the repo and perform the task 
-	 stage("Pull Repo"){
-      git 'https://github.com/farrukh90/cool_website.git'
+		properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')),
+		// Below line triggers this job every minute
+		pipelineTriggers([pollSCM('* * * * * ')]),
+		parameters([
+			// Asks for Environment to Build
+			choice(choices: [
+			'dev1.awsumar.com', 
+			'qa1.awsumar.com', 
+			'stage1.awsumar.com', 
+			'prod1.awsumar.com'], 
+			description: 'Please choose an environment', 
+			name: 'ENVIR'),
+		])
 
-    } 
+		// Pulls a repo from developer
+	stage("Pull Repo"){
+		git 'https://github.com/farrukh90/cool_website.git'
+	}
 		//Installs web server on different environment
 	stage("Install Prerequisites"){
 		sh """
-		ssh centos@jenkins_master.awsumar.com                sudo yum install httpd -y
+		ssh centos@${ENVIR}                 sudo yum install httpd -y
 		"""
 	}
 		//Copies over developers files to different environment
 	stage("Copy artifacts"){
 		sh """
-		scp -r *  centos@jenkins_master.awsumar.com:/tmp
-		ssh centos@jenkins_master.awsumar.com                sudo cp -r /tmp/index.html /var/www/html/
-		ssh centos@jenkins_master.awsumar.com                sudo cp -r /tmp/style.css /var/www/html/
-		ssh centos@jenkins_master.awsumar.com			    sudo chown centos:centos /var/www/html/
-		ssh centos@jenkins_master.awsumar.com			    sudo chmod 777 /var/www/html/*
+		scp -r *  centos@${ENVIR}:/tmp
+		ssh centos@${ENVIR}                 sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@${ENVIR}                 sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@${ENVIR}				    sudo chown centos:centos /var/www/html/
+		ssh centos@${ENVIR}				    sudo chmod 777 /var/www/html/*
 		"""
 	}
 		//Restarts web server
 	stage("Restart web server"){
 		ws("tmp/") {
-			sh "ssh centos@jenkins_master.awsumar.com              sudo systemctl restart httpd"
+			sh "ssh centos@${ENVIR}               sudo systemctl restart httpd"
 		}
 	}
 
