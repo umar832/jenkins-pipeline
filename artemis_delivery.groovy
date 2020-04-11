59ac5a26ecb7afec014d940704151391bc92365f
umar832 <59777479+umar832@users.noreply.github.com>
2020-04-11 11:47:38 -0500

    Create artemis_delivery.groovy

---
+++ artemis_delivery.groovy
@@ -0,0 +1,64 @@
+node {
+	properties(
+		[parameters(
+			[choice(choices: 
+			[
+				'0.1', 
+				'0.2', 
+				'0.3', 
+				'0.4', 
+				'0.5'], 
+	description: 'Which version of the app should I deploy? ', 
+	name: 'Version')])])
+	stage("Stage1"){
+		timestamps {
+			ws {
+                checkout([$class: 'GitSCM', branches: [[name: '${Version}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/artemis.git']]])		}
+	}
+} // we are login to ECR 
+	stage("Get Credentials"){
+		timestamps {
+			ws{
+				sh '''
+                aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 965334959964.dkr.ecr.us-east-1.amazonaws.com/artemis
+					'''
+		    }
+	    }
+    }
+    stage("Build Docker Image"){
+		timestamps {
+			ws {
+				sh '''
+					docker build -t artemis:${Version} .
+					'''
+		    }
+	    }
+    }
+
+    stage("Tag Image"){
+		timestamps {
+			ws {
+				sh '''
+                    docker tag artemis:${Version} 965334959964.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version}
+                    '''
+				}
+			}
+		}
+    stage("Push Image"){
+	    timestamps {
+			ws {
+				sh '''
+					docker push 965334959964.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version}
+					'''
+				}
+			}
+		}
+	stage("Send slack notifications"){
+		timestamps {
+			ws {
+					echo "Slack"
+					//slackSend color: '#BADA55', message: 'Hello, World!'
+				}
+			}
+		}
+	}
